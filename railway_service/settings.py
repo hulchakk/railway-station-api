5a208daf764db0e83b8f4d8537b64ffd626aa460
Dmytro <ys33ys33ys55@gmail.com>
2026-04-16 19:17:09 +0200

    chore: move secret key to the env vars

--- railway_service/settings.py
+++ railway_service/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_env
+
+
+load_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5q+yj@&e&gtcrqq@d(vnsf0dam53h(xh^o!th#e2ldz2a-ygn_"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "YOUR DEFAULT DJANGO SECRET KEY" 
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
