d4c0fcdf6bbdc7fe25cd41d6a3b576d3b1994dc1
Dmytro <ys33ys33ys55@gmail.com>
2026-04-16 19:27:41 +0200

    feat: initialize empty app user

--- railway_service/settings.py
+++ railway_service/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.authtoken"
+    "rest_framework.authtoken",
+    "user",
 ]
 
 MIDDLEWARE = [
