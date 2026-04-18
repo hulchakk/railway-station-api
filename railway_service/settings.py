5854280db9bc9de737731c9edadc74179ebeb82c
Dmytro <ys33ys33ys55@gmail.com>
2026-04-18 15:07:06 +0200

    feat: add throttling

--- railway_service/settings.py
+++ railway_service/settings.py
@@ -142,4 +142,12 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "5/minute",
+        "user": "20/minute"
+    }
 }
