3296f15a8e831517cb6f01e200a8786a3346497a
Dmytro <ys33ys33ys55@gmail.com>
2026-04-18 15:19:15 +0200

    feat(user): add me page

--- user/urls.py
+++ user/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import (
 
 from user.views import (
     CreateUserView,
+    ManageUserView,
 )
 
 
@@ -13,6 +14,7 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 
